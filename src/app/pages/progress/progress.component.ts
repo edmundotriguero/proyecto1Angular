6ff0936d5eb0f2129a039175e06e8af010daacd1
EdmundoTriguero <edmundotriguero@gmail.com>
2018-10-05 15:05:20 -0400

    modulo personalizado

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-progress',
-  templateUrl: './progress.component.html',
+  selector: "app-progress",
+  templateUrl: "./progress.component.html",
   styles: []
 })
 export class ProgressComponent implements OnInit {
-
-  progreso: number = 50;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
   
+  progreso: number = 50;
+  constructor() {}
 
+  ngOnInit() {}
 }
