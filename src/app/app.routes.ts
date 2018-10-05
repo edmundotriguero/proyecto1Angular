9181b0890c85b350c3ac14262b4dc0c9540da5c1
EdmundoTriguero <edmundotriguero@gmail.com>
2018-10-05 09:45:10 -0400

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { RouterModule, Routes } from "@angular/router";
 
-import { DashboardComponent } from "./shared/dashboard/dashboard.component";
+
 import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./login/register.component";
+
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { ProgressComponent } from "./pages/progress/progress.component";
 import { Graficas1Component } from "./pages/graficas1/graficas1.component";
-import { NopagefoundComponent } from "./pages/nopagefound/nopagefound.component";
 import { PagesComponent } from "./pages/pages.component";
-import { RegisterComponent } from "./login/register.component";
+
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
 
 
 const appRoutes: Routes = [
